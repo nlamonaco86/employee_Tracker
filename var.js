6be256b054766a8ab67e866a1b3061e35936efd4
nlamonaco86 <nlamonaco86@gmail.com>
2020-08-21 10:49:02 -0400

    cleaned up the question arrays

--- var.js
+++ var.js
@@ -17,7 +17,29 @@ let empQ = [{
     name: "manager_id"
 }]
 
-let deptQ = [{}]
-let roleQ = [{}]
+let deptQ = [{
+    type: "list",
+    message: "Select a Department:",
+    choices: ["Sales", "Customer Service", "Research & Development", "Legal", "Human & Animal Resources", "Security", "Maintenance"],
+    name: "dept"
+}]
+
+let roleQ = [{
+    type: "list",
+    choices: [1,2,3,4,5],
+    message: "Enter Role ID",
+    name: "role_id"
+},
+{
+    type: "list",
+    choices: ["Employee", "Supervisor", "Manager", "Vice President", "Chief Executive Officer"],
+    message: "Choose a Title",
+    name: "title"
+},
+{
+    type: "input",
+    message: "Salary?",
+    name: "salary"
+}]
 
 module.exports = { empQ, deptQ, roleQ }
