609cc7be61681b2e062bc1b95087ee0a279983d1
nlamonaco86 <nlamonaco86@gmail.com>
2020-08-20 20:45:51 -0400

    main menu works

---
+++ app.js
@@ -0,0 +1,45 @@
+// Dependencies
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+// const connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "Nicky0416!",
+//     database: "cmsdb"
+// });
+
+// launch main menu on startup
+// connection.connect(function (err) {
+//     if (err) throw err;
+//     console.log("connected as id " + connection.threadId);
+//     mainMenu();
+// });
+
+// main menu function
+const mainMenu = () => {
+    return inquirer.prompt([
+        {
+            name: "menuOpt",
+            type: "list",
+            message: "Welcome to ACME Content Management System!",
+            choices: ["ADD", "VIEW", "UPDATE", "EXIT"]
+        }
+    ])
+        .then(response => {
+            //switch case, because smoother than if/else
+            switch (response.menuOpt) {
+                case "ADD":
+                    return console.log("add menu");
+                case "VIEW":
+                    return console.log("view menu");
+                case "UPDATE":
+                    return console.log("update menu");
+                // default:
+                //     connection.end();
+            }
+        });
+}
+
+mainMenu();
